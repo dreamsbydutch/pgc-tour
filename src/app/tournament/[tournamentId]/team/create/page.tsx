5a4a02f7a625a4fd4588ba57480a08fcfff110c7
Curtis Hough <chough14@gmail.com>
2024-12-18 16:19:45 -0500

    remove team folder with old dependencies

--- src/app/tournament/[tournamentId]/team/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { chooseGolfers, seedGolfers } from "@/src/server/api/actions/golfer";
-
-export default function CrateTeamPage({
-  params,
-}: {
-  params: { tournamentId: string };
-}) {
-  return <div>{params.tournamentId}</div>;
-}
