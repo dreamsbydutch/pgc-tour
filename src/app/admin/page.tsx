7996987246cd8c35bae6f76ff3466fb70ef5d92d
Curtis Hough <chough14@gmail.com>
2024-12-26 12:42:10 -0500

    add tour sign up counts to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,16 @@
+import { api } from "@/src/trpc/server";
 import PaymentForm from "./_components/TransactionForm";
 
 export default async function AdminDashboard() {
+  const season = await api.season.getByYear({ year: 2025 });
+  const tours = await api.tour.getBySeason({ seasonID: season?.id });
   return (
     <>
+      <div className="mb-8">
+        {tours.map((tour) => (
+          <div>{`${tour.name} - ${tour.tourCards.length} sign ups (${75 - tour.tourCards.length} left)`}</div>
+        ))}
+      </div>
       <PaymentForm />
     </>
   );
