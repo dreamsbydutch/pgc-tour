e71ad784aa4cf773b5ce7af76eea4cab20e51b7e
Curtis Hough <chough14@gmail.com>
2025-01-12 12:48:21 -0500

    add proper create team page

--- src/app/tournament/[tournamentId]/create-team/page.tsx
+++ src/app/tournament/[tournamentId]/create-team/page.tsx
@@ -2,8 +2,11 @@
 
 import { api } from "@/src/trpc/server";
 import TournamentCountdown from "../../_components/TournamentCountdown";
-import CreateTeamForm from "./CreateTeamForm";
 import LoadingSpinner from "@/src/app/_components/LoadingSpinner";
+import { formatMoney } from "@/src/lib/utils";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+import CreateTeamForm from "./CreateTeamTestForm";
 
 export default async function CreateTeamPage({
   params,
@@ -20,12 +23,23 @@ export default async function CreateTeamPage({
   return (
     <>
       <TournamentCountdown tourney={tournament} key={tournament?.id} />
-      <div>
-        {member?.fullname}
+
+      <div className="flex flex-col items-center font-varela">
+        <Link
+          className="mb-8 flex w-fit flex-row items-center justify-center self-start rounded-md border border-gray-400 px-2 py-0.5"
+          href={`/tournament/${tournament.id}`}
+        >
+          <ArrowLeftIcon size={15} /> Back To Tournament
+        </Link>
+        <div className="text-2xl font-bold">{member?.fullname}</div>
+        <div className="text-xl font-bold">{`${tourCard?.position} - ${tourCard?.points} pts${tourCard?.earnings ? " - " + formatMoney(tourCard?.earnings ?? 0) : ""}`}</div>
+        <div className="mt-6 flex flex-row items-center text-sm text-gray-500">
+          {`Pick your team for the ${tournament.name}`}
+        </div>
         {!tourCard ? (
           <div>You need a Tour Card to pick a team.</div>
         ) : (
-          <CreateTeamForm {...{ tournament }} />
+          <CreateTeamForm tournament={tournament} />
         )}
       </div>
     </>
