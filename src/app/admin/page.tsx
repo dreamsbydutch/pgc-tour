cf1b00c64cf585c3bda9635ba54ef847baa7c201
Curtis Hough <chough14@gmail.com>
2025-01-12 12:36:15 -0500

    add onesignal to admin page for testing

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,34 @@
-import { api } from "@/src/trpc/server";
-import PaymentForm from "./_components/TransactionForm";
+"use client";
 
-export default async function AdminDashboard() {
-  const season = await api.season.getByYear({ year: 2025 });
-  const tours = await api.tour.getBySeason({ seasonID: season?.id });
+import { api } from "@/src/trpc/react";
+import PaymentForm from "./_components/TransactionForm";
+import OneSignal from "react-onesignal";
+import { useEffect } from "react";
+
+export default function AdminDashboard() {
+  useEffect(() => {
+    // Ensure this code runs only on the client side
+    if (typeof window !== "undefined") {
+      OneSignal.init({
+        appId: "e13d2829-191f-493b-8688-486b4520740c",
+        // You can add other initialization options here
+        notifyButton: {
+          enable: true,
+        },
+        // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
+        allowLocalhostAsSecureOrigin: true,
+      });
+    }
+  }, []);
+  const season = api.season.getByYear.useQuery({ year: 2025 }).data;
+  const tours = api.tour.getBySeason.useQuery({ seasonID: season?.id }).data;
   return (
     <>
       <div className="mb-8">
-        {tours.map((tour) => (
+        {tours?.map((tour) => (
           <div
             key={tour.id}
-          >{`${tour.name} - ${tour.tourCards.length} sign ups (${65 - tour.tourCards.length} left)`}</div>
+          >{`${tour.name} - ${tour.tourCards.length} sign ups (${+(process.env.TOUR_MAX_SIZE ?? 75) - tour.tourCards.length} left)`}</div>
         ))}
       </div>
       <PaymentForm />
