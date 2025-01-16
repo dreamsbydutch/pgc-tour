d0b712c19210e9aa73b0f383a7cef5c118c3c989
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2025-01-16 18:06:55 +0000

    fix errors

--- src/app/api/backupTable.ts
+++ src/app/api/backupTable.ts
@@ -2,7 +2,7 @@ import { api } from "@/src/trpc/server";
 import fs from "fs";
 
 export default async function createBackupOfTable() {
-  let users = await api.course.getAll();
+  const users = await api.course.getAll();
   fs.writeFileSync(
     "src/app/api/data/courses.json",
     JSON.stringify(users, null, 4),
