298193ec62fc5c61e253c0f31bb214c3e12b9496
Curtis Hough <chough14@gmail.com>
2025-02-02 14:50:34 -0500

    add update team to router

--- src/server/api/routers/team.ts
+++ src/server/api/routers/team.ts
@@ -62,4 +62,48 @@ export const teamRouter = createTRPCRouter({
       }
       return true;
     }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        score: z.number().optional(),
+        position: z.string().optional(),
+        earnings: z.number().optional(),
+        points: z.number().optional(),
+        round: z.number().optional(),
+        thru: z.number().optional(),
+        today: z.number().optional(),
+        roundOne: z.number().optional(),
+        roundOneTeeTime: z.string().optional(),
+        roundTwo: z.number().optional(),
+        roundTwoTeeTime: z.string().optional(),
+        roundThree: z.number().optional(),
+        roundThreeTeeTime: z.string().optional(),
+        roundFour: z.number().optional(),
+        roundFourTeeTime: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.team.update({
+        where: { id: input.id },
+        data: {
+          score: input.score,
+          position: input.position,
+          earnings: input.earnings,
+          points: input.points,
+          round: input.round,
+          thru: input.thru,
+          today: input.today,
+          roundOne: input.roundOne,
+          roundOneTeeTime: input.roundOneTeeTime,
+          roundTwo: input.roundTwo,
+          roundTwoTeeTime: input.roundTwoTeeTime,
+          roundThree: input.roundThree,
+          roundThreeTeeTime: input.roundThreeTeeTime,
+          roundFour: input.roundFour,
+          roundFourTeeTime: input.roundFourTeeTime,
+        },
+      });
+      return true;
+    }),
 });
