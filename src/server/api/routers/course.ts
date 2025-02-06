7aa378c5f34b4176ed73221a044dbef7126fa592
Curtis Hough <chough14@gmail.com>
2025-02-06 08:27:08 -0500

    update course api for new time change

--- src/server/api/routers/course.ts
+++ src/server/api/routers/course.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { updateSession } from "@/src/lib/supabase/middleware";
 
 export const courseRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     return await ctx.db.course.findMany();
   }),
-  // getById: publicProcedure
-  //   .input(z.object({ courseID: z.string() }))
-  //   .query(async ({ ctx, input }) => {
-  //     return ctx.db.course.findUnique({where: {apiId:input.courseID}});
-  //   }),
+  getById: publicProcedure
+    .input(z.object({ courseID: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.course.findUnique({ where: { id: input.courseID } });
+    }),
   getByName: publicProcedure
     .input(z.object({ name: z.string() }))
     .query(async ({ ctx, input }) => {
@@ -37,6 +38,31 @@ export const courseRouter = createTRPCRouter({
           par: input.par,
           front: input.front,
           back: input.back,
+        },
+      });
+    }),
+  update: publicProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        location: z.string().min(1).optional(),
+        par: z.number().optional(),
+        front: z.number().optional(),
+        back: z.number().optional(),
+        longitude: z.number().optional(),
+        latitude: z.number().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.course.update({
+        where: { name: input.name },
+        data: {
+          location: input.location,
+          par: input.par,
+          front: input.front,
+          back: input.back,
+          longitude: input.longitude,
+          latitude: input.latitude,
         },
       });
     }),
