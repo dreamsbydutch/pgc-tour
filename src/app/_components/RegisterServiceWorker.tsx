b8e6c5a16058f4687963310efca64b9c95f0c62b
Curtis Hough <chough14@gmail.com>
2025-02-14 08:37:28 -0500

    remove service worker code for now

--- src/app/_components/RegisterServiceWorker.tsx
+++ src/app/_components/RegisterServiceWorker.tsx
@@ -1,18 +1,18 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { registerAndSubscribe } from "./Push";
-import { api } from "@/src/trpc/react";
+// import { useEffect } from "react";
+// import { registerAndSubscribe } from "./Push";
+// import { api } from "@/src/trpc/react";
 
-export default function RegisterServiceWorker() {
-  useEffect(() => {
-    const member = api.member.getSelf.useQuery().data;
-    console.log("Registering service worker...");
-    registerAndSubscribe((subs, member) => {
-      console.log("Subscription:", subs);
-    });
-    console.log("Service worker registered.");
-  }, []);
+// export default function RegisterServiceWorker() {
+//   useEffect(() => {
+//     const member = api.member.getSelf.useQuery().data;
+//     console.log("Registering service worker...");
+//     registerAndSubscribe((subs, member) => {
+//       console.log("Subscription:", subs);
+//     });
+//     console.log("Service worker registered.");
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
--- src/app/push/page.tsx
+++ src/app/push/page.tsx
@@ -1,72 +1,72 @@
-"use client";
-import { useEffect, useState } from "react";
-import {
-  checkPermissionStateAndAct,
-  notificationUnsupported,
-  registerAndSubscribe,
-  sendPicksReminder,
-} from "../_components/Push";
-import { api } from "@/src/trpc/react";
+// "use client";
+// import { useEffect, useState } from "react";
+// import {
+//   checkPermissionStateAndAct,
+//   notificationUnsupported,
+//   registerAndSubscribe,
+//   sendPicksReminder,
+// } from "../_components/Push";
+// import { api } from "@/src/trpc/react";
 
-export default function Home() {
-  const [unsupported, setUnsupported] = useState<boolean>(false);
-  const [subscription, setSubscription] = useState<PushSubscription | null>(
-    null,
-  );
-  const [message, setMessage] = useState<string | null>(null);
-  const member = api.member.getSelf.useQuery().data;
-  useEffect(() => {
-    const isUnsupported = notificationUnsupported();
-    setUnsupported(isUnsupported);
-    if (isUnsupported) {
-      return;
-    }
-    checkPermissionStateAndAct(setSubscription, member ?? undefined);
-  }, []);
+// export default function Home() {
+//   const [unsupported, setUnsupported] = useState<boolean>(false);
+//   const [subscription, setSubscription] = useState<PushSubscription | null>(
+//     null,
+//   );
+//   const [message, setMessage] = useState<string | null>(null);
+//   const member = api.member.getSelf.useQuery().data;
+//   useEffect(() => {
+//     const isUnsupported = notificationUnsupported();
+//     setUnsupported(isUnsupported);
+//     if (isUnsupported) {
+//       return;
+//     }
+//     checkPermissionStateAndAct(setSubscription, member ?? undefined);
+//   }, []);
 
-  const tournament = api.tournament.getNext.useQuery().data;
-  const teams = api.team.getByTournament.useQuery({
-    tournamentId: tournament?.id,
-  }).data;
+//   const tournament = api.tournament.getNext.useQuery().data;
+//   const teams = api.team.getByTournament.useQuery({
+//     tournamentId: tournament?.id,
+//   }).data;
 
-  return (
-    <main>
-      <div className="">
-        <button
-          disabled={unsupported}
-          onClick={() =>
-            registerAndSubscribe(setSubscription, member ?? undefined)
-          }
-          className={subscription ? "" : ""}
-        >
-          {unsupported
-            ? "Notification Unsupported"
-            : subscription
-              ? "Notification allowed"
-              : "Allow notification"}
-        </button>
-        {subscription ? (
-          <>
-            <input
-              placeholder={"Type push message ..."}
-              style={{ marginTop: "5rem" }}
-              value={message ?? ""}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={() => sendPicksReminder(tournament, teams)}>
-              Test Web Push
-            </button>
-          </>
-        ) : null}
-        <div className="">
-          <span>Push subscription:</span>
-        </div>
-        <code className="">
-          {subscription
-            ? JSON.stringify(subscription?.toJSON(), undefined, 2)
-            : "There is no subscription"}
-        </code>
-      </div>
-    </main>
-  );
-}
+//   return (
+//     <main>
+//       <div className="">
+//         <button
+//           disabled={unsupported}
+//           onClick={() =>
+//             registerAndSubscribe(setSubscription, member ?? undefined)
+//           }
+//           className={subscription ? "" : ""}
+//         >
+//           {unsupported
+//             ? "Notification Unsupported"
+//             : subscription
+//               ? "Notification allowed"
+//               : "Allow notification"}
+//         </button>
+//         {subscription ? (
+//           <>
+//             <input
+//               placeholder={"Type push message ..."}
+//               style={{ marginTop: "5rem" }}
+//               value={message ?? ""}
+//               onChange={(e) => setMessage(e.target.value)}
+//             />
+//             <button onClick={() => sendPicksReminder(tournament, teams)}>
+//               Test Web Push
+//             </button>
+//           </>
+//         ) : null}
+//         <div className="">
+//           <span>Push subscription:</span>
+//         </div>
+//         <code className="">
+//           {subscription
+//             ? JSON.stringify(subscription?.toJSON(), undefined, 2)
+//             : "There is no subscription"}
+//         </code>
+//       </div>
+//     </main>
+//   );
+// }
