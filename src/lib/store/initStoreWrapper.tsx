e8489a6b6a9105855fc0ff8fe4fe315cc6d94611
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2025-05-29 16:59:28 +0000

    .

--- src/lib/store/initStoreWrapper.tsx
+++ src/lib/store/initStoreWrapper.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect } from "react";
 import { useInitStore } from "./useInitStore";
 
 export default function InitStoreWrapper({children}: {children: React.ReactNode}) {
