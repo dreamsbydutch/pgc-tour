01840c4b7a9401f9fd98c4a0dcb41db7d76a9668
Curtis Hough <chough14@gmail.com>
2025-07-19 15:53:30 -0400

    fix build error

--- src/lib/components/HomePageListings/hooks/useLeaderboardData.ts
+++ src/lib/components/HomePageListings/hooks/useLeaderboardData.ts
@@ -24,7 +24,8 @@ export const useLeaderboardData = () => {
   // Fetch current active tournament
   const currentTournament = tournaments?.find(
     (t) =>
-      t.livePlay || ((t.currentRound ?? 0) > 1 && (t.currentRound ?? 0) < 5),
+      t.livePlay === true ||
+      ((t.currentRound ?? 0) > 1 && (t.currentRound ?? 0) < 5),
   );
 
   // Fetch teams for the current tournament
